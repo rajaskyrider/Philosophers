b41f5a2b26dcf391f4d6e4e30beae6434fbe4fda
Raja Pandiperumal <rpandipe@c2r4s5.42luxembourg.lu>
2024-08-09 17:03:45 +0200

    starting struct

--- philo/init.c
+++ philo/init.c
@@ -1,22 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/08 13:50:00 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/08/09 15:10:37 by rpandipe         ###   ########.fr       */
+/*   Created: 2024/08/09 14:31:27 by rpandipe          #+#    #+#             */
+/*   Updated: 2024/08/09 14:31:38 by rpandipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
-{
-	//t_ph philo;
-
-	if (!check_input(argc, argv))
-		return (FALSE);
-	init(philo, )
-}
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 13:51:08 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/08/09 14:16:12 by rpandipe         ###   ########.fr       */
+/*   Updated: 2024/08/09 15:30:31 by rpandipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,17 @@
 
 typedef struct s_ph
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	dine_count;
+	pthread_t		thread;
+	int				id;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				dine_count;
+	int				*dead_flag;
+	pthread_mutex_t	*rfork;
+	pthread_mutex_t	*lfork;
+	pthread_mutex_t	*write_lock;
+
 }	t_ph;
 
 int	check_input(int argc, char **argv);
